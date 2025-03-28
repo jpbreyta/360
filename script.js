e6a2bc99608e7b21d1e824d70b9e368f1ae60d26
wainoyama <jpreytapogi@gmail.com>
2025-03-28 22:24:57 +0800

    updated

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const viewer = new PhotoSphereViewer.Viewer({
         container: document.getElementById("viewer"),
-        panorama: "./360-wp-plugin.jpg", // Replace with actual 360° image
-        navbar: true,
-        defaultYaw: 0
+        panorama: "360.jpg",
+        navbar: [
+            'autorotate', 'zoom', 'fullscreen'
+        ],
+        defaultZoomLvl: 0,
+        defaultLat: 0,
+        defaultLong: 0,
+        minFov: 30,
+        maxFov: 90
     });
 });
